05d245db7c1f20dcaded2653ab8b6e4f0ed5f90e
ItSushko14 <ItSushkoAndriy@gmail.com>
2023-08-03 21:21:39 +0300

    replace image.js into index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,27 @@ import PhotoGallery from '../components/photogallery/photogallery';
 import {photoblok1, photoblok2, photoblok3} from './images'
 
 export default function Home() {
-    
+
+  
+ const photoblok1 = [
+    { src: "/img/img1.jpg", ratio: 3/2},
+    { src: "/img/img2.jpg", ratio: 3/2},
+    { src: "/img/img4.jpg", ratio: 3/2},   
+]
+
+ const photoblok2 = [
+    { src: "/img/img3.jpg", ratio: 1/1},
+    { src: "/img/img7.jpg", ratio: 1/1},
+    { src: "/img/img9.jpg", ratio: 1/1},   
+]
+
+ const photoblok3 = [
+    { src: "/img/img5.jpg", ratio: 3/2},
+    { src: "/img/img6.jpg", ratio: 3/2},
+    { src: "/img/img8.jpg", ratio: 3/2},   
+]
+
+
   console.log(photoblok1);
   return (
     <div className={styles.container}>
